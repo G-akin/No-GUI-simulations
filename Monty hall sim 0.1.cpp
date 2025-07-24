7f501b5c783a6797eb2073efbee1da48e66c8e8a
G-akin <gakinyele1@gmail.com>
2025-07-24 14:46:52 +0100

    Update Monty hall sim 0.1.cpp

--- Monty hall sim 0.1.cpp
+++ Monty hall sim 0.1.cpp
@@ -4,55 +4,33 @@
 #include <iostream>
 using namespace std;
 
-int randint(int min, int max){
+int get_randint(int min, int max){
    static std::random_device rd;      
     static std::mt19937 gen(rd());     
     std::uniform_int_distribution<> distrib(min, max);
     return distrib(gen);
 }
 
-int main(){   
+int main()
+{   
     int dummyvar;
     int opendoor;
     int guess;
     int wins = 0;
     int itternum;
-    int guess1;
-
     string doors[3] = {"m","g","n"};
-    int indexes[3] = {0,1,2};
-
+    string doors_otherside[3] = {"m","g","n"};
     cout <<"How many iterations with swapping would you like to simulate the monty hall problem for?"<<endl;
     cin >> itternum;
-
     int size = sizeof(doors) / sizeof(doors[0]);
+
     for (int i = 0; i < itternum ; i++) {
-
+        guess = get_randint(0,2);
         random_shuffle(&doors[0],&doors[3]);
-
-        guess1 = randint(0,2);
-        opendoor = randint(0,2);
-
-        if (doors[opendoor] == "m"){
-            opendoor = opendoor + (randint(0,1) * 2 -1);
-
-        if (opendoor>2){
-            opendoor = opendoor - 3;
-
-        } else if(opendoor<0){
-            opendoor = opendoor + 3;
-            }
-        }
-        
-        cout <<"Guess: "<< guess << endl;
-        cout <<"Guess1: "<< guess1 << endl;
-        cout <<"Opendoor: "<< opendoor << endl;
-        cout << endl;
-
-        if (doors[guess] == "m"){ 
-            wins = wins + 1;
-        }
-
+        if (doors[guess] == "m"){
+            wins++;
+        };
+        copy(doors, doors + 3, doors_otherside);
     }
     float winrate = (float)wins/itternum;
     int losses = itternum-wins;
