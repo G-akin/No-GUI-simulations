31e664c6876ce26917496aa44509b076a2a1a44d
G-akin <gakinyele1@gmail.com>
2025-07-24 22:42:45 +0100

    Update Monty hall simulator.py

--- Monty hall simulator.py
+++ Monty hall simulator.py
@@ -1,20 +1,9 @@
 import random
-import time
-
-
-def runtime(start_time):
-    global it
-    current_time = time.time()
-    dt = (current_time - start_time)
-    print("Time taken:",dt,"seconds")
-    print("Rate:",(dt/it)**-1,"Per second")
 
 wins = 0
-#it = 1000
-it=int(input("How many iterations with swapping would you like to simulate the monty hall problem for?"))
-global_start_time = time.time()    
+it=int(input("How many iterations with swapping would you like to simulate the monty hall problem for?"))   
+
 for i in range (1,it+1):
-    #print("iteration:",i)
     doors = ["m","g","n"]
     random.shuffle(doors)
     otherside = list.copy(doors)
@@ -30,11 +19,10 @@ for i in range (1,it+1):
     guess2 = nextglist[0]
     if guess2 == "m":
         wins = wins+1
-    #succesrate = wins/i
-    #print("succesrate:",succesrate)
+
 succesrate = wins/it
 print("Iterations:",(it))
 print("Wins",wins)
 print("losses:",(it-wins))
-print("succesrate:",succesrate*100,"%")
-runtime(global_start_time)
+print("succesrate when swapping:",succesrate*100,"%")
+print("succesrate when not swapping:",100-(succesrate*100),"%")
