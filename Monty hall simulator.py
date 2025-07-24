faa0e6b98f96bb1d72560e492f67c0536f339f78
G-akin <gakinyele1@gmail.com>
2025-07-24 23:04:15 +0100

    Update Monty hall simulator.py

--- Monty hall simulator.py
+++ Monty hall simulator.py
@@ -1,40 +1,35 @@
 import random
-import time
-
-
-def runtime(start_time):
-    global it
-    current_time = time.time()
-    dt = (current_time - start_time)
-    print("Time taken:",dt,"seconds")
-    print("Rate:",(dt/it)**-1,"Per second")
 
 wins = 0
-#it = 1000
-it=int(input("How many iterations with swapping would you like to simulate the monty hall problem for?"))
-global_start_time = time.time()    
+doors = ["m","g","n"]
+it = int(input("How many iterations with swapping would you like to simulate the monty hall problem for?"))   
+
 for i in range (1,it+1):
-    #print("iteration:",i)
-    doors = ["m","g","n"]
     random.shuffle(doors)
-    otherside = list.copy(doors)
-    guess=random.randint(0,2)
-    otherside.pop(guess)
-    if doors[guess]!="m":
-        otherside.remove("m")
- 
-    #presenter
-    nextglist = list.copy(doors)    
-    nextglist.pop(guess)    
-    nextglist.remove(otherside[0])
-    guess2 = nextglist[0]
-    if guess2 == "m":
+    opendoor = random.randint(0,2)
+    guess = random.randint(0,2)
+    
+    for i in range(0,2):
+        if opendoor == guess or doors[opendoor] == "m":
+            opendoor += 1
+
+        if opendoor > 2:
+            opendoor = opendoor-3
+  
+    guess2 = 0
+
+    for i in range(0,2):
+        if guess2 == opendoor or guess2 == guess:
+            guess2 += 1
+        else:
+            break
+
+    if doors[guess2] == "m":
         wins = wins+1
-    #succesrate = wins/i
-    #print("succesrate:",succesrate)
+
 succesrate = wins/it
-print("Iterations:",(it))
-print("Wins",wins)
-print("losses:",(it-wins))
-print("succesrate:",succesrate*100,"%")
-runtime(global_start_time)
+print("Iterations: ",(it))
+print("Wins when swapping: ",wins)
+print("losses when not swapping :",(it-wins))
+print("succesrate when swapping: ",succesrate*100,"%")
+print("succesrate when not swapping:",100-(succesrate*100),"%")
